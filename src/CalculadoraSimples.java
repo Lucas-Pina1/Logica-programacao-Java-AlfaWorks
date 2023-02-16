241f8facc3cb9653205cc14fe98d0ab80f6922f1
Lucas Pina <ti.lucaspina@gmail.com>
2023-02-15 22:20:19 -0300

    Ajustes

--- src/CalculadoraSimples.java
+++ src/CalculadoraSimples.java
@@ -1,60 +1,56 @@
 import java.util.Scanner;
 
 public class CalculadoraSimples {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Digite o primeiro(inteiro) numero da operação: ");
-    Integer primeiroNumeroDigitado = scanner.nextInt();
+        System.out.print("Digite o primeiro(inteiro) numero da operação: ");
+        Integer primeiroNumeroDigitado = scanner.nextInt();
 
-    System.out.print(
-        "Digite o numero referente a que deseja: [1] - Adição / [2] - Subtração / [3] - Multiplicação / [4] - Divisão: ");
-    Integer operacaoEscolhida = scanner.nextInt();
+        System.out.print(
+                "Digite o numero referente a que deseja: [1] - Adição / [2] - Subtração / [3] - Multiplicação / [4] - Divisão: ");
+        Integer operacaoEscolhida = scanner.nextInt();
 
-    System.out.print("Digite o segundo(inteiro) numero da operação: ");
-    Integer segundoNumeroDigitado = scanner.nextInt();
+        System.out.print("Digite o segundo(inteiro) numero da operação: ");
+        Integer segundoNumeroDigitado = scanner.nextInt();
 
-    Boolean adicao = operacaoEscolhida.equals(1);
-    Boolean subtracao = operacaoEscolhida.equals(2);
-    Boolean multiplicacao = operacaoEscolhida.equals(3);
-    Boolean divisao = operacaoEscolhida.equals(4);
+        boolean adicao = operacaoEscolhida.equals(1);
+        boolean subtracao = operacaoEscolhida.equals(2);
+        boolean multiplicacao = operacaoEscolhida.equals(3);
+        boolean divisao = operacaoEscolhida.equals(4);
 
-    Integer resultado = 0;
+        int resultado = 0;
 
-    if (adicao) {
-      Integer operacaoAdicao = primeiroNumeroDigitado + segundoNumeroDigitado;
+        if (adicao) {
 
-      resultado = operacaoAdicao;
+            resultado = primeiroNumeroDigitado + segundoNumeroDigitado;
 
-      System.out.print(
-          "O resultado da soma de " + primeiroNumeroDigitado + " + " + segundoNumeroDigitado + ": " + resultado);
-    } else if (subtracao) {
-      Integer operacaoSubtracao = primeiroNumeroDigitado - segundoNumeroDigitado;
+            System.out.print(
+                    "O resultado da soma de " + primeiroNumeroDigitado + " + " + segundoNumeroDigitado + ": " + resultado);
+        } else if (subtracao) {
 
-      resultado = operacaoSubtracao;
+            resultado = primeiroNumeroDigitado - segundoNumeroDigitado;
 
-      System.out.print(
-          "O resultado da subtração de " + primeiroNumeroDigitado + " - " + segundoNumeroDigitado + ": " + resultado);
-    } else if (multiplicacao) {
-      Integer operacaoMultiplicacao = primeiroNumeroDigitado * segundoNumeroDigitado;
+            System.out.print(
+                    "O resultado da subtração de " + primeiroNumeroDigitado + " - " + segundoNumeroDigitado + ": " + resultado);
+        } else if (multiplicacao) {
 
-      resultado = operacaoMultiplicacao;
+            resultado = primeiroNumeroDigitado * segundoNumeroDigitado;
 
-      System.out.print(
-          "O resultado da multiplicação de " + primeiroNumeroDigitado + " x " + segundoNumeroDigitado + ": "
-              + resultado);
-    } else if (divisao) {
-      Integer operacaoDivisao = primeiroNumeroDigitado / segundoNumeroDigitado;
+            System.out.print(
+                    "O resultado da multiplicação de " + primeiroNumeroDigitado + " x " + segundoNumeroDigitado + ": "
+                            + resultado);
+        } else if (divisao) {
 
-      resultado = operacaoDivisao;
+            resultado = primeiroNumeroDigitado / segundoNumeroDigitado;
 
-      System.out.print(
-          "O resultado da divisão de " + primeiroNumeroDigitado + " / " + segundoNumeroDigitado + ": "
-              + resultado);
-    } else {
-      System.out.println("Não econtramos essa operação, renicie e digite uma opção valida");
+            System.out.print(
+                    "O resultado da divisão de " + primeiroNumeroDigitado + " / " + segundoNumeroDigitado + ": "
+                            + resultado);
+        } else {
+            System.out.println("Não encontramos essa operação, reinicie e digite uma opção valida");
+        }
+
+        scanner.close();
     }
-
-    scanner.close();
-  }
 }
