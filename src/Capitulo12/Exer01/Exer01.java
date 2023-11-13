309e2fe4a44c38b2e5a691a69fa91db54d3f441b
Lucas Pina <ti.lucaspina@gmail.com>
2023-11-13 09:40:29 -0300

    estudo e tentando entender a resolução

--- src/Capitulo12/Exer01/Exer01.java
+++ src/Capitulo12/Exer01/Exer01.java
@@ -8,28 +8,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Exer01 {
+
   public static void main(String[] args) throws IOException {
+    Path arquivo = Paths.get("/temp/alunos.txt");
 
-    Path arquivo = Paths.get("C:/temp/alunos.txt");
+    List<String> nomes = Files.readAllLines(arquivo);
 
-    List<String> linhas = Files.readAllLines(arquivo);
+    ListaAlunos listaAlunos = new ListaAlunos();
 
-    ListaAluno listaAlunos = new ListaAluno();
+    for (int i = 0; i < nomes.size(); i++) {
+      String nome = nomes.get(i);
 
-    for (String nome : linhas) {
-      Alunos aluno = new Alunos(nome);
+      Aluno aluno = new Aluno();
+      aluno.nome = nome;
+
       listaAlunos.adicionar(aluno);
     }
 
     listaAlunos.ordenar();
 
-    Path arquivoOrdenado = Paths.get("C:/temp/alunos.txt");
-
-    List<String> nomesOrdenados = new ArrayList<>();
-    for (Alunos aluno : listaAlunos.getAluno()) {
-      nomesOrdenados.add(aluno.getNome());
+    List<String> nomesOrdenados = new ArrayList<String>();
+    for (int i = 0; i < listaAlunos.tamanho(); i++) {
+      Aluno aluno = listaAlunos.obter(i);
+      nomesOrdenados.add(aluno.nome);
     }
 
-    Files.write(arquivoOrdenado, nomesOrdenados);
+    Path novoArquivo = Paths.get("/temp/alunos_ordenados.txt");
+    Files.write(novoArquivo, nomesOrdenados);
   }
 }
